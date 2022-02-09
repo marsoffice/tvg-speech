83006fd6c59f748a644285518fbe7b98a4ef8690
Alin Florin <alin@qoffice.ro>
2022-02-09 19:13:23 +0200

    -

---
+++ MarsOffice.Tvg.Speech.Abstractions/RequestSpeech.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsOffice.Tvg.Speech.Abstractions
+{
+    public class RequestSpeech
+    {
+        public string VideoId { get; set; }
+        public string JobId { get; set; }
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public IEnumerable<string> Sentences { get; set; }
+        public float? SpeechPitch { get; set; }
+        public float? SpeechSpeed { get; set; }
+        public string SpeechLanguage { get; set; }
+        public string SpeechType { get; set; }
+        public long? SpeechPauseBeforeInMillis { get; set; }
+        public long? SpeechPauseAfterInMillis { get; set; }
+    }
+}
---
+++ MarsOffice.Tvg.Speech.Abstractions/SpeechResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsOffice.Tvg.Speech.Abstractions
+{
+    public class SpeechResult
+    {
+        public string VideoId { get; set; }
+        public string JobId { get; set; }
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public string FileLink { get; set; }
+        public IEnumerable<long> IndividualDurationsInMillis { get; set; }
+        public long TotalDurationInMillis { get; set; }
+    }
+}
