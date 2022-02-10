da4f13a07189d6b3a5397798f14fb3517a04b4ba
Alin Florin <alin@qoffice.ro>
2022-02-10 14:43:06 +0200

    -

---
+++ MarsOffice.Tvg.Speech/Entities/FfProbeResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsOffice.Tvg.Speech.Entities
+{
+    public class FfProbeFormat
+    {
+        public double Duration { get; set; }
+        public long Size { get; set; }
+    }
+
+    public class FfProbeResponse
+    {
+        public FfProbeFormat Format { get; set; }
+    }
+}
--- MarsOffice.Tvg.Speech/RequestSpeechConsumer.cs
+++ MarsOffice.Tvg.Speech/RequestSpeechConsumer.cs
@@ -9,25 +9,35 @@ using Microsoft.CognitiveServices.Speech.Audio;
 using System.IO;
 using System.Diagnostics;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using MarsOffice.Tvg.Speech.Entities;
+using Newtonsoft.Json.Serialization;
+using System.Linq;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
 
 namespace MarsOffice.Tvg.Speech
 {
     public class RequestSpeechConsumer
     {
         private readonly IConfiguration _config;
+        private readonly CloudBlobClient _blobClient;
 
         public RequestSpeechConsumer(IConfiguration config)
         {
             _config = config;
+            var cloudStorageAccount = CloudStorageAccount.Parse(_config["localsaconnectionstring"]);
+            _blobClient = cloudStorageAccount.CreateCloudBlobClient();
         }
 
         [FunctionName("RequestSpeechConsumer")]
         public async Task Run(
-            [QueueTrigger("request-speech", Connection = "localsaconnectionstring")]RequestSpeech request,
+            [QueueTrigger("request-speech", Connection = "localsaconnectionstring")] RequestSpeech request,
             [Queue("speech-result", Connection = "localsaconnectionstring")] IAsyncCollector<SpeechResult> speechResultQueue,
-            
+
             ILogger log)
         {
+            string tempFolderName = null;
             try
             {
                 var speechConfig = SpeechConfig.FromSubscription(_config["speechkey"], _config["location"].Replace(" ", "").ToLower());
@@ -41,43 +51,94 @@ namespace MarsOffice.Tvg.Speech
                 speechConfig.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Audio48Khz192KBitRateMonoMp3);
 
                 using var synthesizer = new SpeechSynthesizer(speechConfig, null);
-                var tempFolderName = Path.GetTempPath() + Guid.NewGuid().ToString();
+                tempFolderName = Path.GetTempPath() + Guid.NewGuid().ToString();
                 Directory.CreateDirectory(tempFolderName);
 
                 var i = 0;
                 var mp3Files = new List<string>();
+                var durations = new List<long>();
 
                 foreach (var sentence in request.Sentences)
                 {
                     var result = await synthesizer.SpeakTextAsync(sentence);
-                    var fileName = $"{i}.mp3";
-                    mp3Files.Add(fileName);
-                    await File.WriteAllBytesAsync(tempFolderName + "/" + $"{i}.mp3", result.AudioData);
+                    var fileName = tempFolderName + "/" + $"{i}.mp3";
+                    mp3Files.Add($"{i}.mp3");
+                    await File.WriteAllBytesAsync(fileName, result.AudioData);
+
+                    var psiFile = new ProcessStartInfo
+                    {
+                        FileName = _config["ffprobepath"],
+                        Arguments = $"-i {i}.mp3 -v quiet -print_format json -show_format -hide_banner",
+                        UseShellExecute = false,
+                        RedirectStandardError = true,
+                        RedirectStandardOutput = true,
+                        WorkingDirectory = tempFolderName
+                    };
+
+                    var processFile = Process.Start(psiFile);
+                    var stdOutFile = processFile.StandardOutput.ReadToEnd();
+                    processFile.WaitForExit((int)TimeSpan.FromSeconds(60).TotalMilliseconds);
+
+                    if (string.IsNullOrEmpty(stdOutFile) || processFile.ExitCode != 0)
+                    {
+                        throw new Exception("Unable to read TTS audio output info");
+                    }
+
+                    var ffProbeResponseFile = JsonConvert.DeserializeObject<FfProbeResponse>(stdOutFile, new JsonSerializerSettings
+                    {
+                        ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                        NullValueHandling = NullValueHandling.Ignore
+                    });
+                    durations.Add((long)Math.Round(ffProbeResponseFile.Format.Duration * 1000));
                     i++;
                 }
 
-                var mp3Chunks = string.Join(" | ", mp3Files);
+                var mp3Chunks = string.Join("|", mp3Files);
                 var psi = new ProcessStartInfo
                 {
                     FileName = _config["ffmpegpath"],
-                    Arguments = $"-i \"concat:{mp3Chunks}\" -acodec copy output.mp3",
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
+                    Arguments = $"-i \"concat:{mp3Chunks}\" -v quiet -hide_banner -acodec copy output.mp3",
                     UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
                     WorkingDirectory = tempFolderName
                 };
 
                 var process = Process.Start(psi);
                 process.WaitForExit((int)TimeSpan.FromSeconds(60).TotalMilliseconds);
 
-                try
+                var outputExists = File.Exists(tempFolderName + "/output.mp3");
+                if (process.ExitCode != 0 || !outputExists)
                 {
-                    Directory.Delete(tempFolderName, true);
-                } catch (Exception ex)
-                {
-                    log.LogError(ex, "Temp folder deletion failed");
+                    throw new Exception("Unable to merge TTS audio files");
                 }
-            } catch (Exception e)
+
+                var blobContainerReference = _blobClient.GetContainerReference("jobsdata");
+#if DEBUG
+                await blobContainerReference.CreateIfNotExistsAsync();
+#endif
+                var blobReference = blobContainerReference.GetBlockBlobReference($"{request.JobId}/tts.mp3");
+           
+                await blobReference.UploadFromFileAsync(tempFolderName + "/output.mp3");
+
+                blobReference.Metadata.Add("IndividualDurationsInMillis", string.Join(",", durations));
+                blobReference.Metadata.Add("TotalDurationInMillis", durations.Sum().ToString());
+                await blobReference.SetMetadataAsync();
+
+                await speechResultQueue.AddAsync(new SpeechResult
+                {
+                    Success = true,
+                    JobId = request.JobId,
+                    VideoId = request.VideoId,
+                    UserEmail = request.UserEmail,
+                    UserId = request.UserId,
+                    IndividualDurationsInMillis = durations,
+                    TotalDurationInMillis = durations.Sum(),
+                    FileLink = blobReference.StorageUri.PrimaryUri.ToString()
+                });
+                await speechResultQueue.FlushAsync();
+            }
+            catch (Exception e)
             {
                 log.LogError(e, "Function threw an exception");
                 await speechResultQueue.AddAsync(new SpeechResult
@@ -90,6 +151,17 @@ namespace MarsOffice.Tvg.Speech
                     UserId = request.UserId
                 });
                 await speechResultQueue.FlushAsync();
+            }
+            finally
+            {
+                try
+                {
+                    Directory.Delete(tempFolderName, true);
+                }
+                catch (Exception ex)
+                {
+                    log.LogError(ex, "Temp folder deletion failed");
+                }
             }
         }
     }
