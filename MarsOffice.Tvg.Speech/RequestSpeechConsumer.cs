c00bd3c7c2a1320f3e52770e900477214745e14b
Alin Florin <alin@qoffice.ro>
2022-02-10 11:55:00 +0200

    d

--- MarsOffice.Tvg.Speech/RequestSpeechConsumer.cs
+++ MarsOffice.Tvg.Speech/RequestSpeechConsumer.cs
@@ -7,13 +7,14 @@ using Microsoft.Extensions.Logging;
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
 using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace MarsOffice.Tvg.Speech
 {
     public class RequestSpeechConsumer
     {
         private readonly IConfiguration _config;
-        private readonly SpeechConfig _speechConfig;
 
         public RequestSpeechConsumer(IConfiguration config)
         {
@@ -40,15 +41,42 @@ namespace MarsOffice.Tvg.Speech
                 speechConfig.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Audio48Khz192KBitRateMonoMp3);
 
                 using var synthesizer = new SpeechSynthesizer(speechConfig, null);
+                var tempFolderName = Path.GetTempPath() + Guid.NewGuid().ToString();
+                Directory.CreateDirectory(tempFolderName);
+
                 var i = 0;
+                var mp3Files = new List<string>();
 
                 foreach (var sentence in request.Sentences)
                 {
                     var result = await synthesizer.SpeakTextAsync(sentence);
-
+                    var fileName = $"{i}.mp3";
+                    mp3Files.Add(fileName);
+                    await File.WriteAllBytesAsync(tempFolderName + "/" + $"{i}.mp3", result.AudioData);
                     i++;
                 }
-                i.ToString();
+
+                var mp3Chunks = string.Join(" | ", mp3Files);
+                var psi = new ProcessStartInfo
+                {
+                    FileName = _config["ffmpegpath"],
+                    Arguments = $"-i \"concat:{mp3Chunks}\" -acodec copy output.mp3",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    WorkingDirectory = tempFolderName
+                };
+
+                var process = Process.Start(psi);
+                process.WaitForExit((int)TimeSpan.FromSeconds(60).TotalMilliseconds);
+
+                try
+                {
+                    Directory.Delete(tempFolderName, true);
+                } catch (Exception ex)
+                {
+                    log.LogError(ex, "Temp folder deletion failed");
+                }
             } catch (Exception e)
             {
                 log.LogError(e, "Function threw an exception");
